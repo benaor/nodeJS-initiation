950cd926f7d12459e5075fbfb950d2824c8fe500
benaor <benjamingirard25@gmail.com>
2021-04-06 22:28:09 +0200

    delete membe

--- _front/app.js
+++ _front/app.js
@@ -19,24 +19,51 @@ app.use(morgan)
 app.use(express.json());
 app.use(express.urlencoded({ extented: true }));
 
-///Routes
+// Routes
+
+//Page d'accueil
 app.get("/", (req, res) => {
-    res.render('index.twig');
+    res.redirect("/members");
 })
 
+//Affichage de tous les membres
 app.get("/members", (req, res) => {
-    instance.get('/members')
-        .then((response) => {
-            if (response.data.status == 'success') {
-                res.render('members.twig', {
-                    members: response.data.result
-                })
-            } else {
-                renderError(res, response.data.message)
-            }
+    apiCall(req.query.max ? "/members?max=" + req.query.max : "/members ", 'get', {}, res, (members) => {
+        res.render('members.twig', {
+            members: members
         })
-        .catch((err) => { renderError(res, err.message) })
+    })
 })
+
+//Recuperation d'un membre
+app.get("/members/:id", (req, res) => {
+    apiCall("/members/" + req.params.id, 'get', {}, res, (member) => {
+        res.render('member.twig', {
+            member: member
+        })
+    })
+})
+
+// Modification d'un membre (GET)
+app.get("/members/:id/edit", (req, res) => {
+    apiCall("/members/" + req.params.id, 'get', {}, res, (result) => {
+        res.render('edit.twig', {
+            member: result
+        })
+    })
+})
+
+// Modification d'un membre (POST)
+app.post("/members/:id/edit", (req, res) => {
+    apiCall("/members/" + req.params.id, 'put', {
+        name: req.body.name
+    }, res, () => {
+        res.redirect("/members")
+    })
+})
+
+// Suppression d'un membre
+
 
 //Lancement de l'application
 app.listen(port, () => console.log("started on port : " + port))
@@ -45,4 +72,19 @@ function renderError(res, errMsg) {
     res.render('error.twig', {
         errorMsg: errMsg
     })
+}
+
+function apiCall(url, method, data, res, next) {
+    instance({
+        method: method,
+        url: url,
+        data: data,
+    }).then((response) => {
+        if (response.data.status == 'success') {
+            next(response.data.result)
+        } else {
+            renderError(res, response.data.message)
+        }
+    })
+        .catch((err) => { renderError(res, err.message) })
 }
